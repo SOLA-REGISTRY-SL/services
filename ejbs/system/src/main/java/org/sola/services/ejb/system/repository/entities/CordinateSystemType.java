ce7dfa722126f066d97f2955dece8427daa986c3
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2016-04-15 17:23:37 +0200

    updated EJBentity for coordinate system

--- ejbs/system/src/main/java/org/sola/services/ejb/system/repository/entities/CordinateSystemType.java
+++ ejbs/system/src/main/java/org/sola/services/ejb/system/repository/entities/CordinateSystemType.java
@@ -5,12 +5,9 @@
  */
 package org.sola.services.ejb.system.repository.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
 import javax.persistence.Table;
 import org.sola.services.common.repository.DefaultSorter;
 import org.sola.services.common.repository.entities.AbstractCodeEntity;
-import org.sola.services.common.repository.entities.AbstractEntity;
 /**
  *
  * @author Moses VB
@@ -18,55 +15,9 @@ import org.sola.services.common.repository.entities.AbstractEntity;
 
 @Table(name = "cordinate_system_type", schema = "system")
 @DefaultSorter(sortString="display_value")
-public class CordinateSystemType extends AbstractEntity{
-    @Id
-    @Column
-    private String code;
-    
-    @Column(name = "display_value")
-    private String displayValue;
-    
-    @Column(name = "status")
-    private String status;
-    
-    @Column(name = "description")
-    private String description;
-    
-    public CordinateSystemType(){
+public class CordinateSystemType extends AbstractCodeEntity {
+    public CordinateSystemType() {
         super();
     }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getDisplayValue() {
-        return displayValue;
-    }
-
-    public void setDisplayValue(String displayValue) {
-        this.displayValue = displayValue;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    
 }
 
